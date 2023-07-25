45df2802d21e8f1a62d1358ddd966fa776bf782a
ElizaBailon <134571511+ElizaBailon@users.noreply.github.com>
2023-07-24 20:35:11 -0500

    Add files via upload

---
+++ Catalogo.cs
@@ -0,0 +1,9 @@
+public class Catalogo
+{
+
+// Variables para almacenar los datos de los componentes del catalogo
+    public int Id {get; set;}
+    public string Componentes{get; set;}
+    public bool Libre{get; set;}
+
+}
---
+++ CompraData.cs
@@ -0,0 +1,8 @@
+public static class CompraData
+{
+
+    // Lista que mostrara cuales son los componentes
+    public static List<Catalogo> Catalogos {get;set;} = new List<Catalogo>();
+    //Lista que muestra las compras hecha
+    public static List<Compra> Compras {get; set;} = new List<Compra>();
+}
